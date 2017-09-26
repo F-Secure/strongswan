F-Secure/strongswan

commit e0e43229736a84db5af6cb7af9cc2ae03da4c906

configure: Detect type of length parameter for gperf generated function
Since 3.1 gperf uses size_t for the length parameter instead of an
unsigned int.

configure.ac
@@ -378,6 +378,21 @@ AC_PATH_PROG([GPERF], [gperf], [], [$PATH:/bin:/usr/bin:/usr/local/bin])
 AC_MSG_CHECKING([gperf version >= 3.0.0])
 if test -x "$GPERF"; then
 	if test "`$GPERF --version | $AWK -F' ' '/^GNU gperf/ { print $3 }' | $AWK -F. '{ print $1 }'`" -ge "3"; then
+		GPERF_OUTPUT="`echo foo | ${GPERF}`"
+		AC_COMPILE_IFELSE(
+			[AC_LANG_PROGRAM(
+				[[#include <string.h>
+				  const char *in_word_set(const char*, size_t); $GPERF_OUTPUT]])],
+			[GPERF_LEN_TYPE=size_t],
+			[AC_COMPILE_IFELSE(
+				[AC_LANG_PROGRAM(
+					[[#include <string.h>
+					  const char *in_word_set(const char*, unsigned); $GPERF_OUTPUT]])],
+				[GPERF_LEN_TYPE=unsigned],
+				[AC_MSG_ERROR([unable to determine gperf len type])]
+			)]
+		)
+		AC_DEFINE_UNQUOTED([GPERF_LEN_TYPE], [$GPERF_LEN_TYPE], [gperf len type])
 		AC_MSG_RESULT([yes])
 	else
 		AC_MSG_RESULT([no])

src/starter/confread.c
@@ -48,7 +48,7 @@ static const char firewall_defaults[] = IPSEC_SCRIPT " _updown iptables";
 /**
  * Provided by GPERF
  */
-extern kw_entry_t *in_word_set (char *str, unsigned int len);
+extern const kw_entry_t *in_word_set(register const char*, register GPERF_LEN_TYPE);
 
 /**
  * Process deprecated keywords
@@ -95,7 +95,7 @@ static void load_setup(starter_config_t *cfg, conf_parser_t *parser)
 {
 	enumerator_t *enumerator;
 	dictionary_t *dict;
-	kw_entry_t *entry;
+	const kw_entry_t *entry;
 	char *key, *value;
 
 	DBG2(DBG_APP, "Loading config setup");
@@ -146,7 +146,7 @@ static void load_ca(starter_ca_t *ca, starter_config_t *cfg,
 {
 	enumerator_t *enumerator;
 	dictionary_t *dict;
-	kw_entry_t *entry;
+	const kw_entry_t *entry;
 	kw_token_t token;
 	char *key, *value;
 
@@ -559,7 +559,7 @@ static void load_conn(starter_conn_t *conn, starter_config_t *cfg,
 {
 	enumerator_t *enumerator;
 	dictionary_t *dict;
-	kw_entry_t *entry;
+	const kw_entry_t *entry;
 	kw_token_t token;
 	char *key, *value;
 

src/libstrongswan/crypto/proposal/proposal_keywords_static.h
@@ -19,7 +19,7 @@
 #include "proposal_keywords.h"
 
 const proposal_token_t* proposal_get_token_static(register const char *str,
-												  register unsigned int len);
+												  register GPERF_LEN_TYPE len);
 
 #endif /* PROPOSAL_KEYWORDS_STATIC_H_ */
 

src/stroke/stroke_keywords.h
@@ -73,6 +73,6 @@ typedef enum {
 
 typedef struct stroke_token stroke_token_t;
 
-extern const stroke_token_t* in_word_set(register const char *str, register unsigned int len);
+extern const stroke_token_t* in_word_set(register const char *str, register GPERF_LEN_TYPE len);
 
 #endif /* _STROKE_KEYWORDS_H_ */

commit 66d04facddb0a6fb609813b213e45866e0cdf47b

configure: Fix gperf length parameter determination
gperf is not actually a build dependency as the generated files are
shipped in the tarball.  So the type depends on the gperf version on
the host that ran gperf and created the tarball, which might not be
the same as that on the actual build host, and gperf might not even
be installed there, leaving the type undetermined.

Fixes: e0e43229736a ("configure: Detect type of length parameter for gperf generated function")

configure.ac
@@ -392,7 +392,7 @@ if test -x "$GPERF"; then
 				[AC_MSG_ERROR([unable to determine gperf len type])]
 			)]
 		)
-		AC_DEFINE_UNQUOTED([GPERF_LEN_TYPE], [$GPERF_LEN_TYPE], [gperf len type])
+		AC_SUBST(GPERF_LEN_TYPE)
 		AC_MSG_RESULT([yes])
 	else
 		AC_MSG_RESULT([no])

src/libstrongswan/.gitignore
@@ -1,2 +1,2 @@
 oid.[ch]
-proposal_keywords_static.c
+proposal_keywords_static.[ch]

src/libstrongswan/Makefile.am
@@ -195,16 +195,19 @@ endif
 EXTRA_DIST = \
 asn1/oid.txt asn1/oid.pl \
 crypto/proposal/proposal_keywords_static.txt \
+crypto/proposal/proposal_keywords_static.h.in \
 plugins/plugin_constructors.py \
 Android.mk
 
 BUILT_SOURCES = \
 $(srcdir)/asn1/oid.c $(srcdir)/asn1/oid.h \
+$(srcdir)/crypto/proposal/proposal_keywords_static.h \
 $(srcdir)/crypto/proposal/proposal_keywords_static.c \
 settings/settings_parser.h
 
 MAINTAINERCLEANFILES = \
 $(srcdir)/asn1/oid.c $(srcdir)/asn1/oid.h \
+$(srcdir)/crypto/proposal/proposal_keywords_static.h \
 $(srcdir)/crypto/proposal/proposal_keywords_static.c
 
 $(srcdir)/asn1/oid.c :	$(srcdir)/asn1/oid.pl $(srcdir)/asn1/oid.txt
@@ -214,6 +217,12 @@ $(srcdir)/asn1/oid.c :	$(srcdir)/asn1/oid.pl $(srcdir)/asn1/oid.txt
 $(srcdir)/asn1/oid.h :	$(srcdir)/asn1/oid.pl $(srcdir)/asn1/oid.txt
 		$(AM_V_GEN) \
 		(cd $(srcdir)/asn1/ && $(PERL) oid.pl)
+
+$(srcdir)/crypto/proposal/proposal_keywords_static.h: $(srcdir)/crypto/proposal/proposal_keywords_static.h.in
+		$(AM_V_GEN) \
+		sed \
+		-e "s:\@GPERF_LEN_TYPE\@:$(GPERF_LEN_TYPE):" \
+		$< > $@
 
 $(srcdir)/crypto/proposal/proposal_keywords_static.c:	$(srcdir)/crypto/proposal/proposal_keywords_static.txt \
 														$(srcdir)/crypto/proposal/proposal_keywords_static.h

src/libstrongswan/crypto/proposal/proposal_keywords_static.h.in
@@ -19,7 +19,7 @@
 #include "proposal_keywords.h"
 
 const proposal_token_t* proposal_get_token_static(register const char *str,
-												  register GPERF_LEN_TYPE len);
+												  register @GPERF_LEN_TYPE@ len);
 
 #endif /* PROPOSAL_KEYWORDS_STATIC_H_ */
 

src/starter/.gitignore
@@ -1,4 +1,5 @@
 starter
+keywords.[ch]
 parser/lexer.c
 parser/parser.[ch]
 parser/parser.output

src/starter/Makefile.am
@@ -39,13 +39,19 @@ starter_LDADD = \
 	libstarter.la \
 	$(SOCKLIB) $(PTHREADLIB) $(ATOMICLIB)
 
-EXTRA_DIST = keywords.txt ipsec.conf ipsec.secrets Android.mk
-MAINTAINERCLEANFILES = keywords.c
-BUILT_SOURCES = keywords.c parser/parser.h
+EXTRA_DIST = keywords.h keywords.txt ipsec.conf ipsec.secrets Android.mk
+MAINTAINERCLEANFILES = keywords.h keywords.c
+BUILT_SOURCES = keywords.h keywords.c parser/parser.h
 
 if USE_LOAD_WARNING
   AM_CPPFLAGS += -DLOAD_WARNING
 endif
+
+keywords.h: $(srcdir)/keywords.h.in
+		$(AM_V_GEN) \
+		sed \
+		-e "s:\@GPERF_LEN_TYPE\@:$(GPERF_LEN_TYPE):" \
+		$< > $@
 
 keywords.c:	$(srcdir)/keywords.txt $(srcdir)/keywords.h
 		$(AM_V_GEN) \

src/starter/confread.c
@@ -46,11 +46,6 @@ static const char esp_defaults[] = "aes128-sha256";
 static const char firewall_defaults[] = IPSEC_SCRIPT " _updown iptables";
 
 /**
- * Provided by GPERF
- */
-extern const kw_entry_t *in_word_set(register const char*, register GPERF_LEN_TYPE);
-
-/**
  * Process deprecated keywords
  */
 static bool is_deprecated(kw_token_t token, char *name, char *conn)

src/starter/keywords.h.in
@@ -196,4 +196,8 @@ struct kw_entry_t {
 	kw_token_t token;
 };
 
+#ifndef IN_GPERF_GENERATED_FILE
+const kw_entry_t *in_word_set(register const char*, register @GPERF_LEN_TYPE@);
+#endif
+
 #endif /* _KEYWORDS_H_ */

src/stroke/.gitignore
@@ -1,1 +1,2 @@
 stroke
+stroke_keywords.[ch]

src/stroke/Makefile.am
@@ -4,12 +4,18 @@ stroke_SOURCES = \
 stroke.c stroke_msg.h stroke_keywords.c stroke_keywords.h
 
 stroke_LDADD = $(top_builddir)/src/libstrongswan/libstrongswan.la $(SOCKLIB)
-EXTRA_DIST = stroke_keywords.txt Android.mk
-BUILT_SOURCES = stroke_keywords.c
-MAINTAINERCLEANFILES = stroke_keywords.c
+EXTRA_DIST = stroke_keywords.h.in stroke_keywords.txt Android.mk
+BUILT_SOURCES = stroke_keywords.h stroke_keywords.c
+MAINTAINERCLEANFILES = stroke_keywords.h stroke_keywords.c
 AM_CPPFLAGS = \
 	-I$(top_srcdir)/src/libstrongswan \
 	-DIPSEC_PIDDIR=\"${piddir}\"
+
+stroke_keywords.h: $(srcdir)/stroke_keywords.h.in
+		$(AM_V_GEN) \
+		sed \
+		-e "s:\@GPERF_LEN_TYPE\@:$(GPERF_LEN_TYPE):" \
+		$< > $@
 
 stroke_keywords.c:	$(srcdir)/stroke_keywords.txt $(srcdir)/stroke_keywords.h
 		$(AM_V_GEN) \

src/stroke/stroke_keywords.h.in
@@ -73,6 +73,7 @@ typedef enum {
 
 typedef struct stroke_token stroke_token_t;
 
-extern const stroke_token_t* in_word_set(register const char *str, register GPERF_LEN_TYPE len);
+extern const stroke_token_t* in_word_set(register const char *str,
+										 register @GPERF_LEN_TYPE@ len);
 
 #endif /* _STROKE_KEYWORDS_H_ */